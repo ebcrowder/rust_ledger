6e87d81ee2eede841c1197bcb248cd696dbe3003
ebcrowder <ebcrowder@gmail.com>
2019-09-14 22:11:54 -0700

    this is fun

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod read_csv;
+mod accounts;
 
 use std::env;
 
@@ -6,7 +6,19 @@ fn main() {
     // collect args into a Vector and assign them to vars
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
+    let command;
 
-    // read contents of CSV and print
-    read_csv::read_csv_to_string(filename);
+    // check for command args - return if none provided
+    if args.len() < 3 {
+        return;
+    } else {
+        command = &args[2];
+    }
+
+    match command.as_ref() {
+        // read contents of CSV and print
+        "accounts" => accounts::read_csv_to_string(filename),
+        // return a message to prompt user for command
+        _ => println!("please provide a command"),
+    }
 }
