22347c962212fe7fdd494e2eeb4689b870b0463a
ebcrowder <ebcrowder@gmail.com>
2019-09-22 16:08:42 -0700

    clean up main arg logic

--- src/balance.rs
+++ src/balance.rs
@@ -1,38 +1,13 @@
 // returns balances of all general ledger accounts
 
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+// use regex::Regex;
+use std::fs;
 
-pub fn balance(filename: &str) -> Result<(), Error> {
-    #[allow(dead_code)]
-    struct AccountSum {
-        account: String,
-        balance: i32,
-    }
+pub fn balance(filename: &str) -> Result<(), std::io::Error> {
+    let file_string = fs::read_to_string(filename).expect("Unable to read ledger file");
+    let account_vec: Vec<&str> = file_string.split_ascii_whitespace().collect();
 
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
-
-    let mut accounts_vec: Vec<AccountSum> = Vec::new();
-
-    for line in reader.lines() {
-        let line_unwrap = line.unwrap();
-
-        let line_vec: Vec<_> = line_unwrap.split_terminator("$").collect();
-
-        accounts_vec.push(AccountSum {
-            account: line_vec[0].parse::<String>().unwrap(),
-            balance: line_vec[1].parse::<i32>().unwrap(),
-        });
-
-        println!("{:?}", line_vec);
-
-
-    }
-
-    // for account in accounts_vec {
-    //     println!("{:?}", account.account)
-    // }
+    println!("{:?}", account_vec);
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,17 @@ use std::env;
 fn main() -> Result<(), std::io::Error> {
     // collect args into a Vector and assign them to vars
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let command;
+    let (filename, command) = parse_args(&args);
 
-    // check for command args - return if none provided
-    if args.len() < 3 {
-        return error::error();
-    } else {
-        command = &args[2];
+    fn parse_args(args: &[String]) -> (&str, &str) {
+        let filename = &args[1];
+        let command = &args[2];
+
+        (filename, command)
     }
 
     // match ledger commands
-    match command.as_ref() {
+    match command {
         "accounts" => accounts::accounts(filename),
         "balance" => balance::balance(filename),
         // return a message to prompt user for command
