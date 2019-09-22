93cb85c9f67016d841ed5d4b9ba38839eb4a691b
ebcrowder <ebcrowder@gmail.com>
2019-09-22 16:32:37 -0700

    ah it sort of works

--- src/balance.rs
+++ src/balance.rs
@@ -1,13 +1,22 @@
 // returns balances of all general ledger accounts
 
-// use regex::Regex;
+use regex::Regex;
 use std::fs;
 
 pub fn balance(filename: &str) -> Result<(), std::io::Error> {
     let file_string = fs::read_to_string(filename).expect("Unable to read ledger file");
-    let account_vec: Vec<&str> = file_string.split_ascii_whitespace().collect();
+    let account_vec: Vec<&str> = file_string.split('\n').collect();
 
-    println!("{:?}", account_vec);
+    let mut results = Vec::new();
+    let date_regex = Regex::new(r"\d{4}/\d{2}/\d{2}").unwrap();
+
+    for line in account_vec {
+        if date_regex.is_match(line) {
+            results.push(line);
+        }
+    }
+
+    println!("{:?}", results);
 
     Ok(())
 }
