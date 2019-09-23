b2cd710c44f301668e16e2c7e11489a69df4dee9
ebcrowder <ebcrowder@gmail.com>
2019-09-22 17:10:38 -0700

    woo hoo

--- src/balance.rs
+++ src/balance.rs
@@ -1,22 +1,38 @@
 // returns balances of all general ledger accounts
 
-use regex::Regex;
+// use regex::Regex;
 use std::fs;
 
 pub fn balance(filename: &str) -> Result<(), std::io::Error> {
     let file_string = fs::read_to_string(filename).expect("Unable to read ledger file");
     let account_vec: Vec<&str> = file_string.split('\n').collect();
+    #[derive(Debug)]
 
-    let mut results = Vec::new();
-    let date_regex = Regex::new(r"\d{4}/\d{2}/\d{2}").unwrap();
+    struct Accounts {
+        account: String,
+        amount: String,
+    }
+
+    let mut transactions_vec: Vec<Accounts> = Vec::new();
 
     for line in account_vec {
-        if date_regex.is_match(line) {
-            results.push(line);
+        if line.contains(':') {
+            let line_vec: Vec<&str> = line.split('\t').collect();
+            let transaction: Vec<&str> = line_vec[0].trim().split_ascii_whitespace().collect();
+
+            if transaction.len() > 1 {
+                let account = transaction[0].to_string();
+                let amount = transaction[1].to_string();
+
+                println!("account {:?}", account);
+                println!("amount {:?}", amount);
+
+                transactions_vec.push(Accounts { account, amount })
+            }
         }
     }
 
-    println!("{:?}", results);
+    println!("{:?}", transactions_vec);
 
     Ok(())
 }
