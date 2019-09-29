8226c61daa790ba9fd3ea9c19f38391c078bf0c2
Eric Crowder <ecrowder@localhost.localdomain>
2019-09-28 21:31:16 -0700

    add another test

--- tests/cli.rs
+++ tests/cli.rs
@@ -16,26 +16,24 @@ fn file_does_not_exist() -> Result<(), std::io::Error> {
 }
 
 #[test]
-fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+fn accounts_test() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = NamedTempFile::new()?;
 
     writeln!(
         file,
         "2019/09/01 * Opening Balance
 	Assets:Checking                   $1000
-	Assets:Savings                         $500
-	Liabilities:Mortgage                $-280000
-	Liabilities:CarLoan                  $-15000
-	Liabilities:Mastercard                      $-500
+	Assets:Savings                     $500
+	Liabilities:Mortgage           $-100000
+	Liabilities:CarLoan             $-15000
+	Liabilities:Creditcard            $-500
 	Equity:Opening Balances
 
 2019/09/03 * (DEBIT) STORMBREAKER BREWING PORTLAND OR
-    ;85369439244194602677984; 05812; ; ; ;
     Expenses:Unknown                             -33
     Equity:Unknown
 
 2019/09/03 * (DEBIT) SQU*SQ *THE GREAT NORT Portland OR
-    ;55432869243200804052309; 05499; ; ; ;
     Expenses:Unknown                           -2.25
     Equity:Unknown
 "
@@ -50,10 +48,43 @@ Equity:Opening
 Equity:Unknown
 Expenses:Unknown
 Liabilities:CarLoan
-Liabilities:Mastercard
+Liabilities:Creditcard
 Liabilities:Mortgage
 ",
     ));
+
+    Ok(())
+}
+
+#[test]
+fn balances_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+
+    writeln!(
+        file,
+        "2019/09/01 * Opening Balance
+	Assets:Checking                   $1000
+	Assets:Savings                     $500
+	Liabilities:Mortgage           $-100000
+	Liabilities:CarLoan             $-15000
+	Liabilities:Creditcard            $-500
+	Equity:Opening Balances
+
+2019/09/03 * (DEBIT) STORMBREAKER BREWING PORTLAND OR
+    Expenses:Unknown                             -33
+    Equity:Unknown
+
+2019/09/03 * (DEBIT) SQU*SQ *THE GREAT NORT Portland OR
+    Expenses:Unknown                           -2.25
+    Equity:Unknown
+"
+    )?;
+
+    let mut cmd = Command::new("./target/debug/rust-ledger");
+    cmd.arg(file.path()).arg("balance");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("total -35.25"));
 
     Ok(())
 }
