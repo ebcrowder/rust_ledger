efd834bfeb97b4b318199c37942e583db457085e
ebcrowder <ebcrowder@gmail.com>
2019-10-12 17:08:01 -0700

    serde refactor

--- src/register_yaml_test.rs
+++ src/register_yaml_test.rs
@@ -2,10 +2,45 @@
 
 extern crate serde_yaml;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Currencies {
+    id: String,
+    name: String,
+    alias: String,
+    note: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct LedgerFile {
+    owner: String,
+    currencies: Currencies,
+    accounts: Vec<Accounts>,
+    transactions: Vec<Transactions>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Accounts {
+    id: i32,
+    name: String,
+    acct_type: String,
+    debit_credit: f64,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Transactions {
+    acct_type: String,
+    desc: String,
+    date: String,
+    debit_credit: f64,
+}
+
 pub fn register_yaml_test(filename: &str) -> Result<(), std::io::Error> {
-    std::fs::File::open("something.yaml")?;
-    let d: String = serde_yaml::from_reader(f)?;
-    println!("Read YAML string: {}", d);
+    let f = std::fs::File::open(filename)?;
+    let d: LedgerFile = serde_yaml::from_reader(f).unwrap();
+
+    println!("Read YAML string: {:?}", d);
 
     Ok(())
 }
