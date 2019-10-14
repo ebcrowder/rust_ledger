95e346e95bc15c6da8e5b7dadbc1d22818de2556
Eric Crowder <ebcrowder@gmail.com>
2019-10-13 21:02:44 -0700

    format terminal output

--- src/register.rs
+++ src/register.rs
@@ -41,12 +41,14 @@ pub fn register(filename: &str) -> Result<(), std::io::Error> {
     let file = std::fs::File::open(filename)?;
     let deserialized_file: LedgerFile = serde_yaml::from_reader(file).unwrap();
 
-    // TODO consider another way to do this
-    println!("date              debit     acct_name                  acct_offset_name");
+    println!(
+        "{0: <10} | {1: <10} | {2: <20} | {3: <20}",
+        "date", "debit", "acct_name", "acct_offset_name"
+    );
 
     for item in deserialized_file.transactions {
         println!(
-            "{:?}      {:?}       {:?}        {:?}",
+            "{0: <10} | {1: <10} | {2: <20} | {3: <20}",
             item.date, item.debit_credit, item.acct_name, item.acct_offset_name
         );
     }
