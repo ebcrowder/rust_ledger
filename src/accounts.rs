8af16a92920efb368f0da5e71c35c8f532c04c8e
Eric Crowder <ebcrowder@gmail.com>
2019-10-18 06:16:16 -0700

    refactor accounts

--- src/accounts.rs
+++ src/accounts.rs
@@ -1,20 +1,60 @@
 // returns all general ledger accounts
 
-use std::fs;
+extern crate serde_yaml;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Currencies {
+    id: String,
+    name: String,
+    alias: String,
+    note: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Accounts {
+    id: i32,
+    acct_name: String,
+    acct_type: String,
+    debit_credit: f64,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Transactions {
+    desc: String,
+    date: String,
+    debit_credit: f64,
+    acct_name: String,
+    acct_offset_name: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct LedgerFile {
+    owner: String,
+    currencies: Currencies,
+    accounts: Vec<Accounts>,
+    transactions: Vec<Transactions>,
+}
+
+struct BalanceAccounts {
+    account: String,
+}
 
 pub fn accounts(filename: &str) -> Result<(), std::io::Error> {
-    let file_string = fs::read_to_string(filename).expect("Unable to read ledger file");
+    let file = std::fs::File::open(filename)?;
+    let deserialized_file: LedgerFile = serde_yaml::from_reader(file).unwrap();
 
-    // split words by separate lines and collect them into a vector
-    let mut account_vec: Vec<&str> = file_string.split_ascii_whitespace().collect();
-    // sort vector and remove duplicates
-    account_vec.sort();
-    account_vec.dedup();
+    let mut account_vec: Vec<BalanceAccounts> = Vec::new();
+
+    for account in deserialized_file.accounts {
+        account_vec.push(BalanceAccounts {
+            account: account.acct_name,
+        });
+    }
 
     for account in account_vec {
-        if account.contains(':') {
-            println!("{}", account);
-        }
+        println!("{:?}", account.account);
     }
 
     Ok(())
