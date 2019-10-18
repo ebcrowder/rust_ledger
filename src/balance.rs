58ef200c024d80f1c392a86db6d5a4d54f5e2297
Eric Crowder <ebcrowder@gmail.com>
2019-10-17 20:17:25 -0700

    update balance and related test to parse yaml files

--- src/balance.rs
+++ src/balance.rs
@@ -1,91 +1,120 @@
 // returns balances of all general ledger accounts
 
+extern crate serde_yaml;
+
 use std::collections::HashMap;
-use std::{
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path,
-};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Currencies {
+    id: String,
+    name: String,
+    alias: String,
+    note: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Accounts {
+    id: i32,
+    acct_name: String,
+    acct_type: String,
+    debit_credit: f64,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Transactions {
+    desc: String,
+    date: String,
+    debit_credit: f64,
+    acct_name: String,
+    acct_offset_name: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct LedgerFile {
+    owner: String,
+    currencies: Currencies,
+    accounts: Vec<Accounts>,
+    transactions: Vec<Transactions>,
+}
+
+struct BalanceAccounts {
+    account_type: String,
+    amount: f64,
+}
+
+struct TransactionAccounts {
+    account: String,
+    amount: f64,
+}
 
 pub fn balance(filename: &str) -> Result<(), std::io::Error> {
-    fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-        let file = File::open(filename).expect("whoops");
-        let buf = BufReader::new(file);
-        buf.lines()
-            .map(|l| l.expect("could not parse line"))
-            .collect()
-    };
+    let file = std::fs::File::open(filename)?;
+    let deserialized_file: LedgerFile = serde_yaml::from_reader(file).unwrap();
 
-    #[derive(Debug)]
+    let mut accounts_vec: Vec<BalanceAccounts> = Vec::new();
+    let mut transactions_vec: Vec<TransactionAccounts> = Vec::new();
 
-    struct Accounts {
-        account: String,
-        amount: f32,
+    // push opening balances into Vec
+    for account in deserialized_file.accounts {
+        accounts_vec.push(BalanceAccounts {
+            account_type: account.acct_type,
+            amount: account.debit_credit,
+        });
     }
 
-    let mut transactions_vec: Vec<Accounts> = Vec::new();
-
-    // TODO add ability to parse beginning equity if amt is not provided
-
-    // iterate through text file and push transactions into vector
-    let lines = lines_from_file(filename);
-    let mut amount: f32 = 0.00;
-
-    println!("{:?}", lines);
-
-    for line in lines {
-        if line.contains(':') {
-            let transaction: Vec<&str> = line.split_ascii_whitespace().collect();
-            let account = transaction[0].to_string();
-
-            if transaction.len() > 1 {
-                amount = transaction[1].parse::<f32>().unwrap();
-            }
-
-            if transaction.len() == 1 {
-                amount = -amount;
-            }
-            transactions_vec.push(Accounts { account, amount });
-        }
+    // push transactions into Vec
+    for transaction in deserialized_file.transactions {
+        transactions_vec.push(TransactionAccounts {
+            account: transaction.acct_name,
+            amount: transaction.debit_credit,
+        })
     }
 
-    // summarize totals by account and place into HashMap
+    // sum totals in accounts and transactions Vecs
+
+    let mut assets_sum: f64 = 0.00;
+    let mut liabilities_sum: f64 = 0.00;
+    let mut equity_sum: f64 = 0.00;
+    let mut income_sum: f64 = 0.00;
+    let mut expenses_sum: f64 = 0.00;
+    let mut check_figure: f64 = 0.00;
+
+    // summarize totals and place into HashMap
     let mut occurrences = HashMap::new();
+    for account in accounts_vec {
+        *occurrences.entry(account.account_type).or_insert(0.00) += account.amount;
+    }
+
     for transaction in transactions_vec {
         *occurrences.entry(transaction.account).or_insert(0.00) += transaction.amount;
     }
 
     // create output
 
-    let mut assets_sum: f32 = 0.00;
-    let mut liabilities_sum: f32 = 0.00;
-    let mut equity_sum: f32 = 0.00;
-    let mut income_sum: f32 = 0.00;
-    let mut expenses_sum: f32 = 0.00;
-    let mut check_figure: f32 = 0.00;
-
     for (key, val) in occurrences.iter() {
-        if key.contains("Assets") {
+        if key.contains("asset") {
             assets_sum += val;
             check_figure += val;
         }
 
-        if key.contains("Liabilities") {
+        if key.contains("liability") {
             liabilities_sum += val;
             check_figure += val;
         }
 
-        if key.contains("Equity") {
+        if key.contains("equity") {
             equity_sum += val;
             check_figure += val;
         }
 
-        if key.contains("Expenses") {
+        if key.contains("expense") {
             expenses_sum += val;
             check_figure += val;
         }
 
-        if key.contains("Income") {
+        if key.contains("income") {
             income_sum += val;
             check_figure += val;
         }
--- tests/cli.rs
+++ tests/cli.rs
@@ -58,36 +58,43 @@ Liabilities:Mortgage
 
 #[test]
 fn balances_test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = NamedTempFile::new()?;
+    let mut file = tempfile::Builder::new().suffix(".yaml").tempfile().unwrap();
 
-    writeln!(
-        file,
-        "2019/09/01 * Opening Balance
-	Assets:Checking                   1000
-	Assets:Savings                     500
-	Liabilities:Mortgage           -100000
-	Liabilities:CarLoan             -15000
-	Liabilities:Creditcard            -500
-	Equity:OpeningBalances          114000
+    let balance_yml = br#"
+        owner: test
+        currencies:
+            id: $
+            name: US Dollar
+            alias: USD
+            note: US Currency 
+        accounts:
+            -   id: 0
+                acct_name: operating
+                acct_type: asset
+                debit_credit: 1500
+            -   id: 1
+                acct_name: equity
+                acct_type: equity
+                debit_credit: -1500
+        transactions:
+            -   acct_name: expense-test-acct
+                debit_credit: 1
+                desc: test
+                date: 2019-01-01
+                acct_offset_name: credit_card
+        "#;
 
-2019/09/03 * (DEBIT) STORMBREAKER BREWING PORTLAND OR
-    Expenses:Unknown                             -33
-    Equity:Unknown
-
-2019/09/03 * (DEBIT) SQU*SQ *THE GREAT NORT Portland OR
-    Expenses:Unknown                           -2.25
-    Equity:Unknown
-"
-    )?;
+    file.write_all(balance_yml).unwrap();
+    file.flush().unwrap();
 
     let mut cmd = Command::new("./target/debug/rust-ledger");
     cmd.arg(file.path()).arg("balance");
     cmd.assert().success().stdout(predicate::str::contains(
         "Assets: 1500.00
-Liabilities: -115500.00
-Equity: 114000.00
+Liabilities: 0.00
+Equity: -1500.00
 Income: 0.00
-Expenses: -35.25",
+Expenses: 1.00",
     ));
 
     Ok(())
