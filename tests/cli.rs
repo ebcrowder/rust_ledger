bd5a7cb1dbc24f199940015dee591f3254f3c752
ebcrowder <ebcrowder@gmail.com>
2019-12-08 16:53:40 -0800

    csv test pass

--- tests/cli.rs
+++ tests/cli.rs
@@ -134,3 +134,65 @@ fn register_test() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn csv_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut yaml_file = tempfile::Builder::new().suffix(".yaml").tempfile().unwrap();
+    let mut csv_file = tempfile::Builder::new().suffix(".csv").tempfile().unwrap();
+
+    let raw_csv = br#"date,transaction,name,memo,amount
+1/1/2000,DEBIT,Grocery store,12345,-55.0000
+1/1/2000,DEBIT,Computer store,12345,-100.9900
+1/1/2000,DEBIT,Gasoline store,12345,-40.0000"#;
+
+    let balance_yml = br#"
+        owner: test
+        currencies:
+            id: $
+            name: US Dollar
+            alias: USD
+            note: US Currency 
+        accounts:
+            -   id: 0
+                acct_name: operating
+                acct_type: asset
+                debit_credit: 1500
+            -   id: 1
+                acct_name: equity
+                acct_type: equity
+                debit_credit: -1500
+        transactions:
+            -   acct_name: expense_general
+                debit_credit: 55
+                acct_type: expense 
+                date: 2000-01-01
+                acct_offset_name: credit_card
+                name: Grocery store 
+            -   acct_name: expense_general
+                debit_credit: 101
+                acct_type: expense 
+                date: 2000-01-01
+                acct_offset_name: credit_card
+                name: Computer store
+            -   acct_name: expense_general
+                debit_credit: 40
+                acct_type: expense 
+                date: 2000-01-01
+                acct_offset_name: credit_card
+                name: Gasoline store
+        "#;
+
+    yaml_file.write_all(balance_yml).unwrap();
+    yaml_file.flush().unwrap();
+
+    csv_file.write_all(raw_csv).unwrap();
+    csv_file.flush().unwrap();
+
+    let mut cmd = Command::new("./target/debug/rust-ledger");
+    cmd.arg(yaml_file.path()).arg(csv_file.path()).arg("csv");
+    cmd.assert().success().stdout(predicate::str::contains(
+        "contents of csv file successfully applied to ledger yaml file",
+    ));
+
+    Ok(())
+}
