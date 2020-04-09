a2786b2f46804eec2806f94a7274ead2a680fb0f
Eric Crowder <ebcrowder@gmail.com>
2020-04-08 20:33:19 -0700

    register refactoring to handle args

--- src/register.rs
+++ src/register.rs
@@ -7,10 +7,17 @@ use num_format::{Locale, ToFormattedString};
 pub fn register(filename: &String, options: &Vec<String>) -> Result<(), std::io::Error> {
     let file = std::fs::File::open(filename)?;
     let deserialized_file: LedgerFile = serde_yaml::from_reader(file).unwrap();
-    let option = match options.len() {
-        0 => "",
-        _ => &options[3],
-    };
+    let mut option = String::new();
+
+    // should filter args - maybe via filter then step_by
+
+    for arg in options {
+        option = if !arg.contains("-") {
+            arg.to_string()
+        } else {
+            "".to_string()
+        };
+    }
 
     println!(
         "{0: <10} | {1: <10} | {2: <20} | {3: <20} | {4: <20}",
@@ -20,15 +27,15 @@ pub fn register(filename: &String, options: &Vec<String>) -> Result<(), std::io:
     let filtered_items: Vec<Transaction> = deserialized_file
         .transactions
         .into_iter()
-        .filter(|x| match option {
+        .filter(|x| match option.as_str() {
             "all" => true,
             _ => {
-                x.acct_type.contains(option)
-                    || x.date.contains(option)
-                    || x.acct_name.contains(option)
-                    || x.acct_offset_name.contains(option)
-                    || x.name.contains(option)
-                    || x.debit_credit.to_string().contains(option)
+                x.acct_type.contains(&option)
+                    || x.date.contains(&option)
+                    || x.acct_name.contains(&option)
+                    || x.acct_offset_name.contains(&option)
+                    || x.name.contains(&option)
+                    || x.debit_credit.to_string().contains(&option)
             }
         })
         .collect();
